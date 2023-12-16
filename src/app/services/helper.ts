f9a0a8bb7577a020188a7198d9c563b324daa10a
rodrigosy03 <74663928@certus.edu.pe>
2023-12-16 12:55:53 -0500

    second commit

--- src/app/services/helper.ts
+++ src/app/services/helper.ts
@@ -1,2 +1,3 @@
-let baserUrl = 'http://localhost:8080'
+// let baserUrl = 'http://localhost:8080'
+let baserUrl = 'http://aa4-backend-env.eba-7ducgmhh.us-east-1.elasticbeanstalk.com'
 export default baserUrl;
